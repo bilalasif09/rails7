6ee8b2fd6dfbd12932767f6077c625b9ca0692a3
Bilal <bilalasif09@gmail.com>
2024-04-09 14:32:08 +0500

    Batch names update in single statement

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,14 @@ if User.count == 0
   User.create([
     {first_name: 'bilal6', last_name: 'asif'},
     {first_name: 'bilal26', last_name: 'asif2'},
-    {first_name: 'bilal3', last_name: 'asif3'}
+    {first_name: 'bilal3', last_name: 'asif3'},
+    {first_name: 'bilal67', last_name: 'asif7'},
+    {first_name: 'bilal268', last_name: 'asif28'},
+    {first_name: 'bilal30', last_name: 'asif39'}
   ])
   procedure_str = AddName.procedure
   ActiveRecord::Base.connection.execute(procedure_str)
   str = AddName.query(3)
-  ActiveRecord::Base.connection.execute(str)
+  puts "query result"
+  puts ActiveRecord::Base.connection.execute(str)
 end
--- lib/add_name.rb
+++ lib/add_name.rb
@@ -10,10 +10,21 @@ class AddName
       DECLARE
         _offset int := (Select count(*) from users where name is not null);
         rec record;
+        str text := 'update users set name = (case id';
+        ids text := '';
+        itr int := 0;
       BEGIN
-        for rec in select first_name, last_name, id from users order by id asc limit _limit offset _offset loop
-          update users set name = concat(rec.first_name, ' ', rec.last_name) where id = rec.id;
+        for rec in select first_name, last_name, id from users limit _limit offset _offset loop
+          itr = itr + 1;
+          str = concat(str, ' when ', rec.id, ' then ', '''', concat(rec.first_name, ' ', rec.last_name), '''');
+          IF itr < _limit THEN
+            ids = concat(ids, rec.id, ',');
+          ELSE
+            ids = concat(ids, rec.id);
+          END IF;
         end loop;
+        str = concat(str, ' else name end) where id in (', ids, ')');
+        EXECUTE str;
       END;
       $$ LANGUAGE plpgsql;
     SQL
